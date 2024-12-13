c0bd266d3393b7c328b4c9d3871fb57a1ee2a044
Alissa <alissa.work17@aol.com>
2024-12-12 20:47:05 -0500

    added repo information functions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // overview is where profile info will appear 
 const overview = document.querySelector(".overview");
-
 const username = "alissa-17";
+const repoList = document.querySelector(".repo-list");
 
 const gitHubInfo = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -26,4 +26,20 @@ const displayUserInfo = function (data) {
     </div>
     `;
   overview.append(div);
+  gitRepos();
 };
+
+const gitRepos = async function(){
+  const repoFetch = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await repoFetch.json();
+  displayRepos(repoData);
+}
+
+const displayRepos = function(repos){
+  for(const repo of repos){
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
+}
